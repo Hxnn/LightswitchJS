140d851e9e13d45c60cacc5ed33e197d5f6ccf1b
Hinn <64025546+Hxnn@users.noreply.github.com>
2022-04-22 11:58:59 +0200

    Color switches, now need button text to switch

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = 'Turn the light on';
+btn.innerHTML = "Turn the light on";
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
-let colors = ['yellow', 'black']
-
+toggle = false;
+    
 btn.addEventListener('click', function(){
-    var colors1 = colors[Math.floor(Math.random() * colors.length)]
-    document.body.style.backgroundColor = colors1;
-})
+  toggle = !toggle;                                     
+  document.body.style.backgroundColor = toggle? "yellow": "black"; 
+}
+
+    ,document.body.style.backgroundColor = "black");
+    btn.addEventListener('click', function change(){
+        if (btn.value=="Turn the light on") btn.value = "Turn the light off";
+        else btn.value = "Turn the light on";
+    })
 // schijf hier tussen je code

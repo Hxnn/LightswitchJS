10478d192921a2104d899dfbbeec72dc60556a3d
Hinn <64025546+Hxnn@users.noreply.github.com>
2022-04-22 11:26:15 +0200

    Added color changes, but it's randomized atm

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.innerHTML = 'Turn the light on';
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
+let colors = ['yellow', 'black']
 
-
-
+btn.addEventListener('click', function(){
+    var colors1 = colors[Math.floor(Math.random() * colors.length)]
+    document.body.style.backgroundColor = colors1;
+})
 // schijf hier tussen je code

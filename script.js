b807d00d500834b5738fa9400cacd6ccc70db78b
Hinn <64025546+Hxnn@users.noreply.github.com>
2022-04-25 10:01:06 +0200

    Text changes

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ btn.innerHTML = "Turn the light on";
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
+
 toggle = false;
     
 btn.addEventListener('click', function(){
   toggle = !toggle;                                     
-  document.body.style.backgroundColor = toggle? "yellow": "black"; 
+  document.body.style.backgroundColor = toggle? "yellow": "black";
+  console.log();
 }
 
     ,document.body.style.backgroundColor = "black");
     btn.addEventListener('click', function change(){
-        if (btn.value=="Turn the light on") btn.value = "Turn the light off";
-        else btn.value = "Turn the light on";
+        if (btn.innerHTML=="Turn the light on") btn.innerHTML = "Turn the light off";
+        else btn.innerHTML = "Turn the light on";
+        console.log();
     })
-// schijf hier tussen je code
+// schijf hier tussen je codeñ

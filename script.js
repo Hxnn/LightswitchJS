8a27a9829f99133605e44dcfeda7660398c448c7
Hinn <64025546+Hxnn@users.noreply.github.com>
2022-04-25 14:37:21 +0200

    Finished

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ toggle = false;
 btn.addEventListener('click', function(){
   toggle = !toggle;                                     
   document.body.style.backgroundColor = toggle? "yellow": "black";
-  console.log();
 }
 
     ,document.body.style.backgroundColor = "black");
     btn.addEventListener('click', function change(){
-        if (btn.innerHTML=="Turn the light on") btn.innerHTML = "Turn the light off";
-        else btn.innerHTML = "Turn the light on";
-        console.log();
+        if (btn.innerHTML=="Turn the light on") {btn.innerHTML = "Turn the light off"; 
+        console.log('The light is on')}
+        
+        else {btn.innerHTML = "Turn the light on"; 
+        console.log('The light is off')}
     })
 // schijf hier tussen je codeñ
